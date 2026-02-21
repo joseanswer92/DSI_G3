e7fed7243a6b4b2e72ad64a723ea99cac62ad0f9
Dorian Wilfred Lanuza Smith <dwls@Dorians-MacBook-Air.local>
2026-02-20 21:32:07 -0600

    Se actualizo main

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,28 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('My API')
-    .setDescription('API description')
-    .setVersion('1.0')
-    .addTag('api')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
+    const config = new DocumentBuilder()
+        .setTitle('My API')
+        .setDescription('API description')
+        .setVersion('1.0')
+        .addTag('api')
+        .addBearerAuth({
+            type: 'http',
+            scheme: 'bearer',
+            bearerFormat: 'JWT',
+            in: 'header',
+            name: 'Authorization',
+            description: 'Enter your bearer token',
+        })
+        .addSecurityRequirements('bearer')
+        .build();
 
-  SwaggerModule.setup('api', app, document);
+    const document = SwaggerModule.createDocument(app, config);
 
-  await app.listen(process.env.PORT ?? 3000);
+    SwaggerModule.setup('api', app, document);
+
+    await app.listen(process.env.PORT ?? 3000);
 }
-void bootstrap();
+bootstrap();
