240ab4158b608a21001e0cb8b69c10936534cb65
Dorian Wilfred Lanuza Smith <dwls@Dorians-MacBook-Air.local>
2026-02-20 21:11:28 -0600

    Se agrego el fix a los archivos de auth controller y auth module asi como auuth service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,24 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private authService: AuthService) {}
 
   @Post('login')
-  login(
-    @Body() data: LoginDto
-  ) {
-    // login logic
+  async login(@Body() data: LoginDto) {
+    const usertoken = await this.authService.validateUser(data);
+
+    if (!usertoken)
+      throw new HttpException('Invalid credentials', HttpStatus.UNAUTHORIZED);
+
+    return usertoken;
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   imports: [
+    PrismaModule,
     JwtModule.register({
       secret: 'your-secret-key', // replace with your secret key
       signOptions: { expiresIn: '1h' }, // token expiration time
