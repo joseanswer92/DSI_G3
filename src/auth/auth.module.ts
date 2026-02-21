f7889f8d7c87d5691e6263c02453246409d3cab8
Dorian Wilfred Lanuza Smith <dwls@Dorians-MacBook-Air.local>
2026-02-20 21:28:42 -0600

    Se agrego el jwt strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,16 +3,19 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
-  imports: [
+   imports: [
+    PassportModule,
     PrismaModule,
     JwtModule.register({
       secret: '454545sssfdfgsfg', // replace with your secret key
       signOptions: { expiresIn: '1h' }, // token expiration time
     }),
   ],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor() {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: '454545sssfdfgsfg',
+        });
+    }
+
+    async validate(payload: any) {
+        console.log('VALIDATE PAYLOAD:', payload);
+        return {
+            userId: payload.id,
+            email: payload.email,
+            role: payload.role,
+        };
+    }
+}
