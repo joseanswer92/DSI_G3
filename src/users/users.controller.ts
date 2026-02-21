0851bf79f5bdf72ad890960b20f7a921bf7e3ce2
Dorian Wilfred Lanuza Smith <dwls@Dorians-MacBook-Air.local>
2026-02-20 21:31:28 -0600

    Se agregaron guards

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -12,7 +13,9 @@ export class UsersController {
     return this.usersService.create(createUserDto);
   }
 
-  @Get()
+   @Get()
+   // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+   @UseGuards(JwtAuthGuard)
   findAll() {
     return this.usersService.findAll();
   }
